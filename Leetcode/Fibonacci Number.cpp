b17d01e3ca7f93858cb6bb58f65099b735d06dcc
Rajnikant Purohit <73100128+Rajni005@users.noreply.github.com>
2022-10-04 17:57:20 +0530

    Update Fibonacci Number.cpp
    Added whole code for the program
    
    Time Complexity: Exponential

--- Leetcode/Fibonacci Number.cpp
+++ Leetcode/Fibonacci Number.cpp
@@ -1,9 +1,20 @@
-class Solution {
-public:
-    int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
-        
-    }
-};
+
+// Fibonacci Series using Recursion
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+ 
+int main()
+{
+    int n = 9;
+    cout << fib(n);
+    getchar();
+    return 0;
+}
+ 
